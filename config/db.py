1ebfdd713d049e17bd71a6e6764b992333ac750f
mohammadjavad2001 <mohammadj200121@gmail.com>
2023-08-15 17:31:11 +0330

    kubernetes

--- config/db.py
+++ config/db.py
@@ -5,10 +5,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base,DeclarativeMeta
 
 password=os.getenv("MYSQL_ROOT_PASSWORD")
-print(password,"      dewdweqqqaa")
-engine = create_engine(f'mysql+pymysql://user:12345789!6mJb@172.28.5.7:3306/HELLOMY')   
+
+print(password,"   dewdweqqqaa")
+
+engine = create_engine(f'mysql+pymysql://user:12345789!6mJb@10.96.5.8:3307/HELLOMY')   
+
 meta = MetaData()
 
 SessionLocal = sessionmaker(bind=engine)
 conn = engine.connect()
-print("DOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNNNNNNNNNNNNNNNNNNNNNNNN")
+
+print("DOWWWWWWWWWWWWWNNNNNNNNNNNNNNNNNNNNNNNNN123587146789641000")
