6b53b44037743e78eae20aba752da989dc497f33
mohammad javad banan <banan_mj@yaftyar.ir>
2023-09-20 10:40:14 +0330

    end

--- app.py
+++ app.py
@@ -581,10 +581,8 @@ def get_current_user(token: Annotated[str,Depends(oauth2_scheme)],db:Session = D
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        print("sdvfsdv")
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
-        print("DSV")
         if username is None:
             raise credentials_exception
         
@@ -607,7 +605,6 @@ def logout(token: Annotated[str,Depends(oauth2_scheme)],db:Session = Depends(get
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
-        print("DSV")
         if username is None:
             raise credentials_exception
     except JWTError:
@@ -690,7 +687,7 @@ def update_user_keycloak(db: Session = Depends(get_db)):
             crud.create_user(db=db,user=user1)
             
         try:
-            
+                                    
             return Response(status_code=response.status_code,content=response.json())
         except Exception:
             return Response(content=response.content,status_code=response.status_code)
@@ -702,12 +699,12 @@ from datetime import datetime
 
 scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job("interval", minutes=30)  # Adjust the interval as needed
+@scheduler.scheduled_job("interval", minutes=2)  # Adjust the interval as needed
 def synchronize_users(db: Session = Depends(get_db)):
     try:
         now = datetime.now()
+        
         print(f"Synchronization started at {now}")
-        # Retrieve user data from Keycloak and update your database
         update_user_keycloak(db=db)
         print(f"Synchronization completed at {now}")
     except Exception as e:
